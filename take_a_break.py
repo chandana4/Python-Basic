fa14e5989ef78edbfc53ab773f3deec194a44ad2
CHANDANA S <38815@user>
2016-11-01 14:01:51 -0400

    Take a break first session with new url

--- take_a_break.py
+++ take_a_break.py
@@ -1,5 +1,5 @@
 import webbrowser
 import time
 while(1):
-	webbrowser.open("https://www.google.co.in/search?q=take+a+break+gif&client=firefox-a&rls=org.mozilla:en-US:official&channel=fflb&tbm=isch&imgil=HjCGsVuSuO56_M%253A%253BHBIpKH1fqojj7M%253Bhttp%25253A%25252F%25252Fwww.loralujames.com%25252F2014%25252F05%25252Ftime-out.html&source=iu&pf=m&fir=HjCGsVuSuO56_M%253A%252CHBIpKH1fqojj7M%252C_&usg=__8t6gdm7mkZ37Sjy3sw4Kb3zrOuU%3D&biw=1440&bih=742&ved=0ahUKEwi9mszpxIfQAhXCNY8KHRsXBqcQyjcINg&ei=vIcYWL3MOsLrvASbrpi4Cg#imgrc=HjCGsVuSuO56_M%3A")
+	webbrowser.open_new("https://www.google.co.in/search?q=take+a+break+gif&client=firefox-a&rls=org.mozilla:en-US:official&channel=fflb&tbm=isch&imgil=HjCGsVuSuO56_M%253A%253BHBIpKH1fqojj7M%253Bhttp%25253A%25252F%25252Fwww.loralujames.com%25252F2014%25252F05%25252Ftime-out.html&source=iu&pf=m&fir=HjCGsVuSuO56_M%253A%252CHBIpKH1fqojj7M%252C_&usg=__8t6gdm7mkZ37Sjy3sw4Kb3zrOuU%3D&biw=1440&bih=742&ved=0ahUKEwi9mszpxIfQAhXCNY8KHRsXBqcQyjcINg&ei=vIcYWL3MOsLrvASbrpi4Cg#imgrc=HjCGsVuSuO56_M%3A")
 	time.sleep(20*60*60)
