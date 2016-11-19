cf1ece9aa80fcd33a71658a72c95efaefeceb535
CHANDANA S <38815@user>
2016-11-19 10:07:16 -0500

    A Simple madlib generator added

---
+++ Silly-Mad-Lib-Gen.py
@@ -0,0 +1,4 @@
+name=raw_input("Enter your name: ")
+animal=raw_input("Enter an animal that you are scared of : ")
+verb=raw_input("Enter a verb")
+print "Welcome to the scary room",name,"whispers",animal," You open your eyes slowly and try to recollect what happened!\nYou've never been to this place before.The",animal," stares at you from the ceiling and ",verb," around you. You see that the room is still quite dark and smells foul.Then you hear many voices, saying, ",name,",",name," So, you pull the blanket above you and wait for someone to save."
