3cbf3651e6655b1e69422795a88392012d705609
Chandana <chaandana4@gmail.com>
2019-02-14 20:41:58 +0530

    Add files via upload

---
+++ dict.py
@@ -0,0 +1,25 @@
+list=[1,2,3]
+Dict = {} 
+print(Dict) 
+  
+# with Integer Keys 
+Dict = {1: 'Like', 2: 'Subscribe', 3: 'Share'} 
+print("\nDictionary with Integer Keys: ") 
+print(Dict) 
+print(Dict[2]) 
+
+# with Mixed keys 
+Dict = {'Name': 'Chandana', 1: [1, 2, 3, 4]} 
+print("\nDictionary with the use of Mixed Keys: ") 
+print(Dict) 
+print(Dict['Name']) 
+# with dict() method 
+Dict = dict({1: 'Hello', 2: 'Comment', 3:'Down'}) 
+print("\nDictionary with the use of dict(): ") 
+print(Dict) 
+
+# with each item as a Pair 
+Dict = dict([(1, 'Dict'), (2, 'pair')]) 
+print("\nDictionary with each item as a pair: ") 
+print(Dict) 
+
